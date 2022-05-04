1e671f5294a8d2533cf5c5f0eaeae7f6ab3d3700
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-05-04 17:37:21 -0500

    Test pasado del método getAllStudents

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -4,6 +4,6 @@ describe("Tests para StudentService", () => {
     test("Lectura de todos los estudiantes de visualpartner.json", () => {
         const vpsStudents=Reader.readJsonFile("visualpartners.json")
         const allStudents=StudentService.getAllStudents(vpsStudents)
-        expect(allStudents.length).toBe(50);
+        expect(allStudents.length).toBe(51);
     });
 })
