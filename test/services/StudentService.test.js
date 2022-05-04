02637a274a6bdad29f7fb13e633a2eedf900179e
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-05-04 17:47:55 -0500

    Test para getAccreditedStudents (Fail)

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -11,4 +11,9 @@ describe("Tests para StudentService", () => {
         const emailStudents=StudentService.getEmailOfCertifiedStudents(vpsStudents)
         expect(emailStudents.length).toBe(29);
     });
+    test("Lectura de los nombres de todos los estudiantes de visualpartner.json con credits mayor a 500", () => {
+        const vpsStudents=Reader.readJsonFile("visualpartners.json")
+        const creditsStudents=StudentService.getAccreditedStudents(vpsStudents)
+        expect(creditsStudents.length).toBe(29);
+    });
 })
