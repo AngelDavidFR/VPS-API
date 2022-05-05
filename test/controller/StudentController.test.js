14d848cc88eac2f2bb7b2ff45ebfceaa23e83d0e
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-05-05 18:36:43 -0500

    Agregando formato con linter

--- test/controller/StudentController.test.js
+++ test/controller/StudentController.test.js
@@ -1,16 +1,16 @@
 
-const StudentController=require("../../lib/controller/StudentController")
+const StudentController=require("../../lib/controller/StudentController");
 describe("Tests para StudentController", () => {
     test("Lectura de todos los estudiantes de visualpartner.json", () => {
-        const allStudents=StudentController.getAllStudents()
+        const allStudents=StudentController.getAllStudents();
         expect(allStudents.length).toBe(51);
     });
     test("Lectura de los emails de todos los estudiantes de visualpartner.json con certificación", () => {
-        const emailStudents=StudentController.getEmailOfCertifiedStudents()
+        const emailStudents=StudentController.getEmailOfCertifiedStudents();
         expect(emailStudents.length).toBe(29);
     });
     test("Lectura de los nombres de todos los estudiantes de visualpartner.json con credits mayor a 500", () => {
-        const creditsStudents=StudentController.getAccreditedStudents()
+        const creditsStudents=StudentController.getAccreditedStudents();
         expect(creditsStudents.length).toBe(27);
     });
-})
+});
--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,19 +1,19 @@
-const Reader=require("../../lib/utils/Reader")
-const StudentService=require("../../lib/services/StudentService")
+const Reader=require("../../lib/utils/Reader");
+const StudentService=require("../../lib/services/StudentService");
 describe("Tests para StudentService", () => {
     test("Lectura de todos los estudiantes de visualpartner.json", () => {
-        const vpsStudents=Reader.readJsonFile("visualpartners.json")
-        const allStudents=StudentService.getAllStudents(vpsStudents)
+        const vpsStudents=Reader.readJsonFile("visualpartners.json");
+        const allStudents=StudentService.getAllStudents(vpsStudents);
         expect(allStudents.length).toBe(51);
     });
     test("Lectura de los emails de todos los estudiantes de visualpartner.json con certificación", () => {
-        const vpsStudents=Reader.readJsonFile("visualpartners.json")
-        const emailStudents=StudentService.getEmailOfCertifiedStudents(vpsStudents)
+        const vpsStudents=Reader.readJsonFile("visualpartners.json");
+        const emailStudents=StudentService.getEmailOfCertifiedStudents(vpsStudents);
         expect(emailStudents.length).toBe(29);
     });
     test("Lectura de los nombres de todos los estudiantes de visualpartner.json con credits mayor a 500", () => {
-        const vpsStudents=Reader.readJsonFile("visualpartners.json")
-        const creditsStudents=StudentService.getAccreditedStudents(vpsStudents)
+        const vpsStudents=Reader.readJsonFile("visualpartners.json");
+        const creditsStudents=StudentService.getAccreditedStudents(vpsStudents);
         expect(creditsStudents.length).toBe(27);
     });
-})
+});
