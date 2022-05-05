f1836cbac9543744a37d58bd45d4a06c2741eeb4
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-05-05 17:46:15 -0500

    Test de StudentController pasadas

--- test/controller/StudentController.test.js
+++ test/controller/StudentController.test.js
@@ -3,14 +3,14 @@ const StudentController=require("../../lib/controller/StudentController")
 describe("Tests para StudentController", () => {
     test("Lectura de todos los estudiantes de visualpartner.json", () => {
         const allStudents=StudentController.getAllStudents()
-        expect(allStudents.length).toBe(53);
+        expect(allStudents.length).toBe(51);
     });
     test("Lectura de los emails de todos los estudiantes de visualpartner.json con certificación", () => {
         const emailStudents=StudentController.getEmailOfCertifiedStudents()
-        expect(emailStudents.length).toBe(27);
+        expect(emailStudents.length).toBe(29);
     });
     test("Lectura de los nombres de todos los estudiantes de visualpartner.json con credits mayor a 500", () => {
         const creditsStudents=StudentController.getAccreditedStudents()
-        expect(creditsStudents.length).toBe(21);
+        expect(creditsStudents.length).toBe(27);
     });
 })
